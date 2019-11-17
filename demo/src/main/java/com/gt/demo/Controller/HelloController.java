7edfa03bd731e3343fb04333fae96310d210a4ae
guo-tao <18793464155@163.com>
2019-11-17 11:39:51 +0800

    服务注册

--- demo/src/main/java/com/gt/demo/Controller/HelloController.java
+++ demo/src/main/java/com/gt/demo/Controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.gt.demo.Controller;
 
 import com.gt.demo.model.Book;
+import com.netflix.discovery.DiscoveryClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @Autowired
-    Book book;
+    private Book book;
+
+//    @Autowired
+//    private DiscoveryClient client;
 
     @RequestMapping("/hello")
     public String index(){
